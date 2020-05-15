ad507f4068a020443a3322c5652b2ee2b8323c26
Adnan Hakim <adnanhakim007@gmail.com>
2020-05-16 00:21:35 +0530

    Added controller

--- src/main/java/com/adnanhakim/courseapi/api/TopicController.java
+++ src/main/java/com/adnanhakim/courseapi/api/TopicController.java
@@ -1,4 +1,21 @@
 package com.adnanhakim.courseapi.api;
 
+import com.adnanhakim.courseapi.model.Topic;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class TopicController {
+
+    @GetMapping(path = "{topics}")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "The Spring Framework provides a comprehensive programming and configuration model for modern Java-based enterprise applications - on any kind of deployment platform."),
+                new Topic("java", "Core Java", "Java is a general-purpose programming language that is class-based, object-oriented, and designed to have as few implementation dependencies as possible."),
+                new Topic("javascript", "JavaScript", "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm.")
+        );
+    }
 }
---
+++ src/main/java/com/adnanhakim/courseapi/model/Topic.java
@@ -0,0 +1,29 @@
+package com.adnanhakim.courseapi.model;
+
+public class Topic {
+
+    private String id;
+    private String name;
+    private String description;
+
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
